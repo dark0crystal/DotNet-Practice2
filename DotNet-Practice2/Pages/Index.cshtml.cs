da3dfb200c7cc0e281de931fb34afcafd57893d3
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-05 22:00:15 +0400

    sentence spliting form

--- DotNet-Practice2/Pages/Index.cshtml.cs
+++ DotNet-Practice2/Pages/Index.cshtml.cs
@@ -14,5 +14,17 @@ public class IndexModel : PageModel
 
     public void OnGet()
     {
+        
+    }
+
+    public void OnPost()
+    {
+        if (HttpContext.Request.Query["sentence"].Count > 0)
+        {
+            string sentence =Request.Form["sentence"];
+            ViewData["sentence"] = sentence;
+            string splitCharacter = Request.Form["split-character"];
+            Console.WriteLine(sentence);
+        }
     }
 }
